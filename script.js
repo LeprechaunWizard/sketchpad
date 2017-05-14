3e961258e4e16a71865129da56e3b3da31b17026
Dieter Verhufen <dieterverhufen@rogers.com>
2017-05-14 11:40:22 -0400

    Added control buttons and random color option

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 var size = 16
+var rainbow = false;
+
 $(document).ready(function() {
     // Create the grid
     createGrid();
 
     // Change color when mouseover
     $('.panel').mouseenter(function() {
-        $(this).css('background-color', 'black');
+        if(rainbow === true) {
+            $(this).css('background-color', randomColor());
+        } else {
+            $(this).css('background-color', 'black');
+        }
+
     });
 
     // Clear grid
     $("#clear").click(function() {
-        
         $('.panel').css('background-color', 'white');
+    });
+
+    // toggle rainbow
+    $('#random').click(function() {
+        toggleRainbow();
     });
 
 });
@@ -30,5 +41,23 @@ function createGrid() {
 function createRow($div) {
     for (k = 0; k < size; k++) {
         $div.append('<div class="panel"></div>');
+    }
+}
+
+function randomColor() {
+    var letterArray = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
+    var color = '#';
+
+    for(i = 0; i < 6; i++) {
+        color += letterArray[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function toggleRainbow() {
+    if (rainbow === false) {
+        rainbow = true;
+    } else {
+        rainbow = false;
     }
 }
